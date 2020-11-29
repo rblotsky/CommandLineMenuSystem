74357736b090ee024bd66abbe9e8599978a9d8af
rblotsky <roman.blotsky@gmail.com>
2020-11-29 16:19:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandLineMenuSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
